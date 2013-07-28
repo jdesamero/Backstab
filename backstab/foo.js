39902e0d97ebff6fb2061809e0db416a5b6ace04
Joel Desamero <joel@geekoracle.com>
2013-07-28 09:54:26 -0400

    Tweak demo and Foo sub-class

--- backstab/foo.js
+++ backstab/foo.js
@@ -21,10 +21,26 @@
 	//
 	_.extend( Backstab.Foo.prototype, Backstab.Events, {
 		
-		initialize: function() { },
+		initialize: function( opts ) {
+			this.options = opts;
+		},
 		
 		foo: function( msg ) {
-			alert( 'Foo ' + msg + '!!!' );
+			
+			var alrt = 'Calling Foo!!!';
+			
+			if ( this.options.foo ) {
+				alrt += ' (this.options.foo -> ' + this.options.foo + ')';
+			}
+
+			if ( msg ) {
+				alrt += ' (msg -> ' + msg + ')';
+			}
+			
+			alert( alrt );
+			
+			this.trigger( 'foo', this, msg );
+			return this;
 		}
 				
 	} );
